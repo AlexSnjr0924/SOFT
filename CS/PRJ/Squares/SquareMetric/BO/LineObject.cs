636612a047e3c8bce7b23c10b70ef3d54b3aec5c
AlexSnjr0924 <snjr@yandex.ru>
2025-01-23 17:37:24 +0300

    Add files via upload

--- CS/PRJ/Squares/SquareMetric/BO/LineObject.cs
+++ CS/PRJ/Squares/SquareMetric/BO/LineObject.cs
@@ -1,1 +1,106 @@
+﻿using SquareMetric.BL;
 
+namespace SquareMetric.BO {
+  /// <summary>Structures, used for geometry shapes (1 measure)</summary>
+  public static class LineObject {
+    #region... /* ShapeStruct. Objects */
+    /// <summary>Point on infinity 2-dim space</summary>
+    public class Point {
+      /// <summary>Coordinate X</summary>
+      public decimal X { get; set;}
+
+      /// <summary>Coordinate Y</summary>
+      public decimal Y { get; set;}
+
+      /// <summary>Coordinate quarter for point</summary>
+      public int Quarter { get; set;}
+
+      /// <summary>Move point on infinity square</summary>
+      /// <param name="movX">Movement by coordinate X</param>
+      /// <param name="movY">Movement by coordinate Y</param>
+      public void Move(decimal movX = 0, decimal movY = 0) {
+        // Move point
+        if (movX != 0) { X += movX;}
+        if (movY != 0) { Y += movY;}
+
+        // Determine coordinate quarter
+        Quarter = X < 0 ? Y < 0 ? 3 : 2 : Y < 0 ? 4 : 1;
+      }
+
+      /// <summary>Constructor</summary>
+      /// <param name="pX">Start X coordinate</param>
+      /// <param name="pY">Start Y coordinate</param>
+      public Point(decimal pX = 0, decimal pY = 0) {
+        // Init coordinates, quarter
+        X = pX;
+        Y = pY;
+        Move();
+      }
+    }
+
+    /// <summary>Section (finite line) between 2 points</summary>
+    public class Line {
+      /// <summary>First end of segment (can be skipped on input)</summary>
+      public Point PointA { get; set;}
+
+      /// <summary>Second end of segment (can be skipped on input)</summary>
+      public Point PointB { get; set;}
+
+      /// <summary>Length of segment (can be input directly or calculated as distance between points)</summary>
+      public decimal Length { get; set;}
+
+      /// <summary>Move line on infinity square</summary>
+      /// <param name="movX">Movement by coordinate X</param>
+      /// <param name="movY">Movement by coordinate Y</param>
+      public void Move(decimal movX = 0, decimal movY = 0) {
+        // Move points
+        if (movX != 0 || movY != 0) { 
+          PointA.Move(movX, movY);
+          PointB.Move(movX, movY);
+        }
+      }
+
+      /// <summary>Init line features</summary>
+      /// <param name="pX1">Point A. Coordinate X</param>
+      /// <param name="pY1">Point A. Coordinate Y</param>
+      /// <param name="pX2">Point B. Coordinate X</param>
+      /// <param name="pY2">Point B. Coordinate Y</param>
+      public void Init(decimal pX1, decimal pY1, decimal pX2, decimal pY2) {
+        // Init edge points in line
+        PointA = new Point(pX1, pY1);
+        PointB = new Point(pX2, pY2);
+
+        // Length of finite line
+        Length = MathAccount.Distance(pX1, pY1, pX2, pY2);
+      }
+
+      /// <summary>Constructor</summary>
+      /// <param name="pX1">Point A. Coordinate X</param>
+      /// <param name="pY1">Point A. Coordinate Y</param>
+      /// <param name="pX2">Point B. Coordinate X</param>
+      /// <param name="pY2">Point B. Coordinate Y</param>
+      public Line(decimal pX1, decimal pY1, decimal pX2, decimal pY2) { Init(pX1, pY1, pX2, pY2);}
+
+      /// <summary>Constructor (II)</summary>
+      /// <param name="pPointA">Point A as object</param>
+      /// <param name="pPointB">Point B as object</param>
+      public Line(Point pPointA, Point pPointB) { Init(pPointA.X, pPointA.Y, pPointB.X, pPointB.Y);}
+    }
+
+    /// <summary>Plane angle</summary>
+    public struct StrAngle {
+      /// <summary>Angle value in radians, will be calculated after finding trihonometric (co)sine value</summary>
+      public decimal ValueRad { get; set;}
+
+      /// <summary>Angle value in degrees (value-in-radians * pi / 2)</summary>
+      public decimal ValueDeg { get; set;}
+
+      /// <summary>Sine of angle (vector-product divided by side-length-product)</summary>
+      public decimal ASine { get; set;}
+
+      /// <summary>Cosine of angle (scalar-product divided by side-length-product)</summary>
+      public decimal ACosine { get; set;}
+    }
+    #endregion /* ShapeStruct. Objects */
+  }
+}
---
+++ CS/PRJ/Squares/SquareMetric/BO/SquareObject.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using SquareMetric.BA;
+using SquareMetric.BL;
+using Punct = SquareMetric.BO.LineObject.Point;
+using Side = SquareMetric.BO.LineObject.Line;
+
+namespace SquareMetric.BO {
+  /// <summary>Structures, used for geometry shapes (2 measures)</summary>
+  public static class SquareObject {
+    /// <summary>C.0. Abstract geometry figure</summary>
+    public class Figure {
+      /// <summary>Square of figure</summary>
+      public decimal Square { get; set;}
+
+      /// <summary>Each geometry shape have (significant) points, even a circle has central point</summary>
+      public Punct[] FPoints { get; set;}
+
+      /// <summary>Virtual method. Calculate square</summary>
+      protected virtual void GetSquare() { Console.WriteLine("(operation) Figure.GetSquare");}
+
+      /// <summary>Move geometry shape by 2-dim space</summary>
+      /// <param name="movX">Movement by coordinate X</param>
+      /// <param name="movY">Movement by coordinate Y</param>
+      public void Move(decimal movX = 0, decimal movY = 0) {
+        // Move each figure point
+        Console.WriteLine("(operation) Figure.Move");
+        for (var i = 0; i < FPoints.Length; i++) { FPoints[i].Move(movX, movY);}
+      }
+    }
+
+    /// <summary>C.1. Circle. Set of points, which are on the same distance from centre</summary>
+    public class Circle: Figure {
+      /// <summary>Center of circle</summary>
+      public Punct Center { get { return FPoints[0];}}
+
+      /// <summary>Radius of circle</summary>
+      public decimal Radius { get; set;}
+
+      /// <summary>Realize method. Calculate measure of round</summary>
+      protected override void GetSquare() {
+        Console.WriteLine("(operation) Circle.GetSquare");
+        Square = MathAccount.Numeric_Pi * Radius * Radius;
+      }
+
+      /// <summary>Constructor</summary>
+      /// <param name="pCX">Centre of circle, X</param>
+      /// <param name="pCY">Centre of circle, Y</param>
+      /// <param name="pRadius">Radius of circle</param>
+      public Circle(Punct pCentre, decimal pRadius) {
+        // Init main circle features
+        Console.WriteLine("(creation) Circle.Constructor");
+        FPoints = new Punct[] { pCentre };
+        Radius = pRadius;
+        GetSquare();
+      }
+
+      /// <summary>Determine, does the circle covers a point</summary>
+      /// <param name="pPoint">Point on 2-dim flat as object</param>
+      /// <returns>-1: circle does not cover point; 0: C. touches P.; 1: P. lies in C. entirely</returns>
+      public int Cover(Punct pPoint) {
+        // Evaluate distance between point and circle radius
+        var pointDist = CalculateFlat.GetDistance(FPoints[0], pPoint);
+        return pointDist < Radius ? 1 : pointDist > Radius ? -1 : 0;
+      }
+    }
+
+    /// <summary>C.2. Polygion. Figure with several sides (and same amount of vertex)</summary>
+    public class BasePolygon: Figure {
+      /// <summary>Sides (lines between vertexes) in polygon</summary>
+      public Side[] FSides { get; set;}
+
+      /// <summary>Perimeter of polygon</summary>
+      public decimal Perimeter { get; set;}
+
+      /// <summary>Move polygon by 2-dim space</summary>
+      /// <param name="movX">Movement by coordinate X</param>
+      /// <param name="movY">Movement by coordinate Y</param>
+      public new void Move(decimal movX = 0, decimal movY = 0) {
+        // Move each figure point and side
+        for (var i = 0; i < FPoints.Length; i++) {
+          FPoints[i].Move(movX, movY);
+          FSides[i].Move(movX, movY);
+        }
+      }
+
+      /// <summary>Virtual method. Calculate square</summary>
+      protected override void GetSquare() { Console.WriteLine("(operation) BasePolygon.GetSquare");}
+
+      /// <summary>Constructor</summary>
+      /// <param name="pCoords">List of figure point coordinates (3 for triangle, 4 for rectangle,...)</param>
+      public BasePolygon(List<Punct> pCoords) {
+        // Info
+        int sideCnt = pCoords.Count;
+        Console.WriteLine("(creation) BasePolygon.Constructor. Sides: {0}", sideCnt);
+
+        // Create array for points and sides
+        FPoints = new Punct[sideCnt];
+        FSides = new Side[sideCnt];
+        Perimeter = 0m;
+
+        for (int i = 0; i < sideCnt; i++) {
+          // New point of polygon
+          FPoints[i] = pCoords[i];
+
+          // New side of polygon
+          if (i > 0) {
+            FSides[i - 1] = new Side(pCoords[i - 1], pCoords[i]);
+            Perimeter += FSides[i - 1].Length;
+          }
+        }
+
+        // Last side of polygon
+        FSides[sideCnt - 1] = new Side(pCoords[sideCnt - 1], pCoords[0]);
+        Perimeter += FSides[sideCnt - 1].Length;
+
+        // Calculate square of just-created figure
+        GetSquare();
+      }
+    }
+
+    /// <summary>C.2.1. Geometrical figure. Triangle</summary>
+    public class Triangle: BasePolygon {
+      /// <summary>Constructor</summary>
+      public Triangle(List<Punct> pCoords): base(pCoords) {}
+
+      /// <summary>Triangle point coordinates. x1</summary>
+      public decimal X1 => FPoints[0].X;
+
+      /// <summary>Triangle point coordinates. y1</summary>
+      public decimal Y1 => FPoints[0].Y;
+
+      /// <summary>Triangle point coordinates. x2</summary>
+      public decimal X2 => FPoints[1].X;
+
+      /// <summary>Triangle point coordinates. y2</summary>
+      public decimal Y2 => FPoints[1].Y;
+
+      /// <summary>Triangle point coordinates. x3</summary>
+      public decimal X3 => FPoints[2].X;
+
+      /// <summary>Triangle point coordinates. y3</summary>
+      public decimal Y3 => FPoints[2].Y;
+
+      /// <summary>Calculate square of triangle</summary>
+      protected override void GetSquare() {
+        // Square by determinant; sine of angle difference
+        Console.WriteLine("(operation) Triangle.GetSquare");
+        Square = MathAccount.Abs(MathAccount.VectorProduct(
+            FPoints[1].X - FPoints[0].X, FPoints[1].Y - FPoints[0].Y
+          , FPoints[2].X - FPoints[0].X, FPoints[2].Y - FPoints[0].Y) / 2);
+      }
+
+      /// <summary>Calculate triangle square by method of Heron. For illustrative purposes</summary>
+      public decimal GetSquareHeron() {
+        // Start multiplier p/2
+        Console.WriteLine("(operation) Triangle.GetSquareHeron");
+        var pHalf = Perimeter / 2;
+        var result = pHalf;
+
+        // Get product of differences between p/2 and each triangle side
+        for (int i = 0; i < 3; i++) { result *= pHalf - FSides[i].Length;}
+        return MathAccount.Root2(result);
+      }
+    }
+
+    /// <summary>C2.2. Geometrical figure. Polygon with 4 or more sides</summary>
+    public class Polygon: BasePolygon {
+      /// <summary>Constructor</summary>
+      public Polygon(List<Punct> pCoords): base(pCoords) {}
+
+      /// <summary>Calculate square of polygon</summary>
+      protected override void GetSquare() {
+        // Square by Gauss (land surveyor) formula
+        Console.WriteLine("(operation) Polygon.GetSquare");
+        Square = 0m;
+
+        // Square by Gauss. Cycle
+        for (int i = 0; i < FPoints.Length; i++) {
+          var idx0 = i == 0 ? FPoints.Length - 1 : i - 1;
+          var idx1 = i == FPoints.Length - 1 ? 0 : i + 1;
+          Square += FPoints[i].Y * (FPoints[idx0].X - FPoints[idx1].X);
+        }
+
+        // Previous measures were doubled
+        Square = MathAccount.Abs(Square / 2.0m); 
+      }
+    }
+  }
+}
